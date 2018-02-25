8498595b60eab43e9829bf80448a99205c57e333
chikuwa111 <take0shun1111@gmail.com>
2018-02-25 23:20:11 +0900

    Remove content-script.js

--- src/content-script.js
+++
@@ -1,5 +0,0 @@
-var body = document.getElementsByTagName('body')[0];
-var script = document.createElement('script');
-script.setAttribute('type', 'text/javascript');
-script.setAttribute('src', chrome.extension.getURL('/script.js'));
-body.appendChild(script);
--- src/script.js
+++ src/script.js
@@ -1,28 +1,17 @@
-var head = document.getElementsByTagName('head')[0];
-var style = document.createElement('style');
-head.appendChild(style);
+const head = document.getElementsByTagName('head')[0]
+const style = document.createElement('style')
+head.append(style)
 
-var body = document.getElementsByTagName('body')[0];
-var select = document.createElement('select');
-select.setAttribute('style', 'position: fixed; bottom: 60px; right: 20px;');
-select.innerHTML = '' +
-  '<option selected value="9"></option>' +
-  '<option value="1">1</option>' +
-  '<option value="2">2</option>' +
-  '<option value="3">3</option>' +
-  '<option value="4">4</option>' +
-  '<option value="5">5</option>' +
-  '<option value="6">6</option>' +
-  '<option value="7">7</option>' +
-  '<option value="8">8</option>';
+const select = document.createElement('select')
+select.setAttribute('style', 'position: fixed; bottom: 60px; right: 20px;')
+select.innerHTML = '<option selected value="9"></option>' +
+  Array.from({length: 8}, (_, i) => `<option value="${i + 1}">${i + 1}</option>`).join('')
 
-select.addEventListener('change', function() {
-  var indent = parseInt(select.value);
-  var css = '';
-  for (i = indent; i <= 8; i++) {
-    css += ' .listindent' + i + ' { display: none; } ';
-  }
-  style.innerHTML = css;
-});
+select.addEventListener('change', () => {
+  const v = parseInt(select.value)
+  const css = Array.from({length: 9 - v}, (_, i) => `.listindent${i + v} {display: none;}`).join(' ')
+  style.innerText = css
+})
 
-body.appendChild(select);
+const body = document.getElementsByTagName('body')[0]
+body.append(select)
